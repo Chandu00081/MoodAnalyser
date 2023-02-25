0ffe41895c03db2650a61d536cee75fd8e16ea75
chandu00081 <chandu00081@gmail.com>
2023-02-25 18:55:31 +0530

    UC! added

---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalyser
+    {
+        public string message;
+
+        public MoodAnalyser()
+        {
+        }
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood(string message)
+        {
+            if (message.ToUpper().Contains("HAPPY"))
+            {
+                return "HAPPY";
+            }
+            else
+            {
+                return "SAD";
+            }
+        }
+    }
+}
